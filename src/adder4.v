5e7399702742dd291d1324535a8198e415589d59
tannercastanha <152110354+tannercastanha@users.noreply.github.com>
2026-09-21 17:43:42 -0700

    Refactor carry handling in adder4.v

--- src/adder4.v
+++ src/adder4.v
@@ -14,41 +14,40 @@ module adder4(
    
 // add your code here -- you must use four instances of the full adder "fulladd", defined below
 
-	wire cout[3:0];
+	wire [2:0] carry;
 	
 	fulladd addc0 (
 		.x(a[0]),
 		.y(b[0]),
 		.cin(1'b0),
-		.sum(result[0],
-		.cout(cout[0])
+		.sum(result[0]),
+		.cout(carry[0])
 		);
 
 	fulladd addc1 (
 		.x(a[1],
 		.y(b[1]),
-		.cin(cout[0]),
+		.cin(carry[0]),
 		.sum(result[1]),
-		.cout(cout[1])
+		.cout(carry[1])
 		);
 
 	fulladd addc2 (
 		.x(a[2],
 		.y(b[2]),
-		.cin(cout[1]),
+		.cin(carry[1]),
 		.sum(result[2]),
-		.cout(cout[2])
+		.cout(carry[2])
 		);
 
 	fulladd addc3 (
 		.x(a[3],
 		.y(b[3]),
-		.cin(cout[2]),
+		.cin(carry[2]),
 		.sum(result[3]),
-		.cout(cout[3])
+		.cout(result[4])
 		);
 
-		assign result[4] = .cout[3];
 endmodule
 
 module fulladd(
