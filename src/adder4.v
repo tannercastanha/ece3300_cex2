636cd19922cbce9a6c4e038e1a759b65992d78d2
tannercastanha <152110354+tannercastanha@users.noreply.github.com>
2026-09-21 17:25:50 -0700

    Add 4-bit adder implementation using fulladd
    Implemented a 4-bit adder using four instances of the full adder module.

--- src/adder4.v
+++ src/adder4.v
@@ -13,7 +13,37 @@ module adder4(
 	      output [4:0] result);
    
 // add your code here -- you must use four instances of the full adder "fulladd", defined below
-   
+
+	wire cout[3:0];
+	
+	fulladd addc0 (
+		.x(a[0]),
+		.y(b[0]),
+		.cin(1'b0),
+		.sum(result[0],
+		.cout(cout[0]));
+
+	fulladd addc1 (
+		.x(a[1],
+		.y(b[1]),
+		.cin(cout[0]),
+		.sum(result[1]),
+		.cout(cout[1]));
+
+	fulladd addc2 (
+		.x(a[2],
+		.y(b[2]),
+		.cin(cout[1]),
+		.sum(result[2]),
+		.cout(cout[2]));
+
+	fulladd addc3 (
+		.x(a[3],
+		.y(b[3]),
+		.cin(cout[2]),
+		.sum(result[3]),
+		.cout(cout[3]));
+	
 endmodule
 
 module fulladd(
